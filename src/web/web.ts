febeec41671cb708c1f0114b048486330c41f214
Eugene Rikhter <devilr33f@gmail.com>
2024-09-21 20:09:27 +0600

    feat: encrypt contacts in api response

--- src/web/web.ts
+++ src/web/web.ts
@@ -1,11 +1,17 @@
+import { createCipheriv } from 'node:crypto'
+
 import fastify from 'fastify'
 
 import { tg } from '@/bot/bot.js'
+import config from '@/config.js'
 
 export const server = fastify()
 
 server.get('/contacts', async (_, res) => {
   const contacts = await tg.getContacts()
 
-  res.send(contacts)
+  const cipher = createCipheriv('aes-256-cbc', Buffer.from(config.crypto.key, 'hex'), Buffer.from(config.crypto.iv, 'hex'))
+  const encrypted = Buffer.concat([cipher.update(contacts.map((contact) => contact.id).join(',')), cipher.final()])
+
+  res.send({ contacts: encrypted })
 })
